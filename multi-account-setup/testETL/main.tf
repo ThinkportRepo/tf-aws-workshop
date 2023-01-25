beaabd7156edbd469dd8aeee911ca4b7e11729e3
Timea Magyar <tmagyar@thinkport.digital>
2023-01-25 23:46:39 +0100

    format tf files

--- multi-account-setup/testETL/main.tf
+++ multi-account-setup/testETL/main.tf
@@ -8,17 +8,17 @@ terraform {
 
   # Configure remote backend with state locking
   backend "s3" {
-    bucket = "ou-anm4-0exbp2yg-tf-remote-backend"
-    key    = "testETL/terraform.tfstate"
-    region = "eu-central-1"
+    bucket         = "ou-anm4-0exbp2yg-tf-remote-backend"
+    key            = "testETL/terraform.tfstate"
+    region         = "eu-central-1"
     dynamodb_table = "ou-anm4-0exbp2yg-tf-s3-state-lock"
   }
 }
 
 locals {
   account_id = {
-    prod :    597575188840
-    dev :     868312938057
+    prod : 597575188840
+    dev : 868312938057
     staging : 232021966246
   }
 
@@ -42,11 +42,11 @@ module "glue_job" {
 
   create = var.create_job
 
-  name   = local.job_name
-  role_arn = module.glue_job_role.arn
+  name            = local.job_name
+  role_arn        = module.glue_job_role.arn
   script_location = module.s3_script_bucket.s3_bucket_arn
 
-  connections = var.job_connections
+  connections  = var.job_connections
   max_capacity = var.job_max_capacity
   arguments = {
     S3_TARGET_BUCKET = module.s3_target_bucket.s3_bucket_arn
@@ -59,7 +59,7 @@ module "s3_target_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   bucket = local.job_target_bucket
 
-  acl    = "private"
+  acl = "private"
 
   # Auto-delete objects in bucket
   force_destroy = true
@@ -85,7 +85,7 @@ module "s3_tmp_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   bucket = local.job_tmp_bucket
 
-  acl    = "private"
+  acl = "private"
 
   # Auto-delete objects in bucket
   force_destroy = true
@@ -111,7 +111,7 @@ module "s3_script_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   bucket = local.job_script_bucket
 
-  acl    = "private"
+  acl = "private"
 
   # Auto-delete objects in bucket
   force_destroy = true
@@ -143,7 +143,7 @@ resource "aws_s3_object" "job_script" {
 
 
 module "glue_job_role" {
-  source  = "cloudposse/iam-role/aws"
+  source = "cloudposse/iam-role/aws"
   # pin module to a specific version
   version = "0.17.0"
 
@@ -169,8 +169,8 @@ module "glue_job_role" {
 # Define Glue Job Policy
 data "aws_iam_policy_document" "s3_access" {
   statement {
-    sid       = "FullAccess"
-    effect    = "Allow"
+    sid    = "FullAccess"
+    effect = "Allow"
     resources = [
       "arn:aws:s3:::${local.job_script_bucket}",
       "arn:aws:s3:::${local.job_script_bucket}/*",
